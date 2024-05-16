f07c44327ff6c43821c16c884a0117c0b5b9f0ec
Davi Ximenes <82062555+davixmns@users.noreply.github.com>
2024-05-16 14:56:21 -0300

    :construction: migrations

---
+++ OasisAPI/Migrations/20240515191444_rename_UserId.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OasisAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class rename_UserId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_oasis_chats_oasis_users_UserId",
+                table: "oasis_chats");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "oasis_chats",
+                newName: "OasisUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_oasis_chats_UserId",
+                table: "oasis_chats",
+                newName: "IX_oasis_chats_OasisUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_oasis_chats_oasis_users_OasisUserId",
+                table: "oasis_chats",
+                column: "OasisUserId",
+                principalTable: "oasis_users",
+                principalColumn: "OasisUserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_oasis_chats_oasis_users_OasisUserId",
+                table: "oasis_chats");
+
+            migrationBuilder.RenameColumn(
+                name: "OasisUserId",
+                table: "oasis_chats",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_oasis_chats_OasisUserId",
+                table: "oasis_chats",
+                newName: "IX_oasis_chats_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_oasis_chats_oasis_users_UserId",
+                table: "oasis_chats",
+                column: "UserId",
+                principalTable: "oasis_users",
+                principalColumn: "OasisUserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
